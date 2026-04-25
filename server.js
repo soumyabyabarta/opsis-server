6d046b3410a16af65ed6d942e5bf8a976787edf9
soumyabyabarta <byabartasoumya@gmail.com>
2026-04-25 15:29:49 +0530

    Fixed trust proxy for Render and rate limiter

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import sessionRoutes from './routes/session.routes.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB Atlas
